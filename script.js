2d0457547fd2dc15ea2b927b3e869a689a5c3523
Zachary McQueen <Zack.McQueen@ucsf.edu>
2024-01-02 11:17:14 -0800

    edit text

--- script.js
+++ script.js
@@ -7,13 +7,21 @@ function calculateDaysRemaining() {
     return daysRemaining > 0 ? daysRemaining : 0;
 }
 
-function displayDaysRemaining() {
+function calculatePercentageOfYear(days) {
+    // There are 365 days in a non-leap year
+    return ((days / 365) * 100).toFixed(2);
+}
+
+function displayCountdown() {
     const days = calculateDaysRemaining();
+    const percentage = calculatePercentageOfYear(days);
+
     document.getElementById('days-remaining').innerText = days + " days";
+    document.getElementById('percentage-year').innerText = "which is " + percentage + "% of a calendar year.";
 }
 
 // Update the countdown every day (86400000 milliseconds)
-setInterval(displayDaysRemaining, 86400000);
+setInterval(displayCountdown, 86400000);
 
 // Initialize
-displayDaysRemaining();
+displayCountdown();
